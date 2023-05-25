587d3ed9234264dcf4e089bf100124b1af89fa49
Juan MP <jsmontes@uniboyaca.edu.co>
2023-05-24 23:31:01 -0500

    Se programo la función de modificar para estudiantes y actividades

---
+++ views/accion_modificar_actividad.php
@@ -0,0 +1,27 @@
+<?php
+require '../models/actividad.php';
+require '../controllers/conexionDbController.php';
+require '../controllers/baseController.php';
+require '../controllers/actividadController.php';
+
+use actividad\Actividad;
+use actividadController\ActividadController;
+
+$actividad = new Actividad();
+$actividad->setId($_POST['id']);
+$actividad->setDescripcion($_POST['descripcion']);
+$actividad->setNota($_POST['nota']);
+$actividad->setCodigoEstudiante($_POST['codigoEstudiante']);
+
+$actividadController = new ActividadController();
+$resultado = $actividadController->update($actividad->getCodigo(), $actividad);
+if ($resultado) {
+    echo '<h1>Actividad modificada</h1>';
+} else {
+    echo '<h1>No se pudo modificar la actividad</h1>';
+}
+?>
+<br>
+<a href="../index.php">Volver al inicio</a>
+
+
---
+++ views/accion_modificar_estudiante.php
@@ -0,0 +1,26 @@
+<?php
+require '../models/estudiante.php';
+require '../controllers/conexionDbController.php';
+require '../controllers/baseController.php';
+require '../controllers/estudianteController.php';
+
+use estudiante\Estudiante;
+use estudianteController\EstudianteController;
+
+$estudiante = new Estudiante();
+$estudiante->setCodigo($_POST['codigo']);
+$estudiante->setNombres($_POST['nombres']);
+$estudiante->setApellidos($_POST['apellidos']);
+
+$estudianteController = new EstudianteController();
+$resultado = $estudianteController->update($estudiante->getCodigo(), $estudiante);
+if ($resultado) {
+    echo '<h1>Estudiante modificado</h1>';
+} else {
+    echo '<h1>No se pudo modificar el estudiante</h1>';
+}
+?>
+<br>
+<a href="../index.php">Volver al inicio</a>
+
+
