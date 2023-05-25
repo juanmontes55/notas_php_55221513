95d9e0d9c24f151131b9ef92a2bb36cfea968e8a
Juan MP <jsmontes@uniboyaca.edu.co>
2023-05-24 23:32:27 -0500

    Se programo la función de registrar para estudiantes y actividades

---
+++ views/accion_registro_actividad.php
@@ -0,0 +1,21 @@
+<?php
+require '../models/actividad.php';
+require '../controllers/conexionDbController.php';
+require '../controllers/baseController.php';
+require '../controllers/actividadController.php';
+
+use actividad\Actividad;
+use actividadController\ActividadController;
+
+$actividad = new Actividad();
+$actividad->setDescripcion($_POST['descripcion']);
+$actividad->setNota($_POST['nota']);
+$actividad->setCodigoEstudiante($_POST['codigoEstudiante']);
+
+$actividadController = new ActividadController();
+$resultado = $actividadController->create($actividad);
+if ($resultado) {
+    echo '<h1>Actividad registrado</h1>';
+} else {
+    echo '<h1>No se pudo registrar la actividad</h1>';
+}
---
+++ views/accion_registro_estudiante.php
@@ -0,0 +1,21 @@
+<?php
+require '../models/estudiante.php';
+require '../controllers/conexionDbController.php';
+require '../controllers/baseController.php';
+require '../controllers/estudianteController.php';
+
+use estudiante\Estudiante;
+use estudianteController\EstudianteController;
+
+$estudiante = new Estudiante();
+$estudiante->setCodigo($_POST['codigo']);
+$estudiante->setNombres($_POST['nombres']);
+$estudiante->setApellidos($_POST['apellidos']);
+
+$estudianteController = new EstudianteController();
+$resultado = $estudianteController->create($estudiante);
+if ($resultado) {
+    echo '<h1>Estudiante registrado</h1>';
+} else {
+    echo '<h1>No se pudo registrar el estudiante</h1>';
+}
