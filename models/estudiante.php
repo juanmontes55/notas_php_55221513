1b9f1877f1718009706c6f1a60c6103d0a6f162e
Juan MP <jsmontes@uniboyaca.edu.co>
2023-05-24 23:18:06 -0500

    Se agrego la carpeta de modelo, junto a los respectivos archivos de actividad y estudiante con sus respectivos gets y sets

---
+++ models/estudiante.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace estudiante;
+
+class Estudiante
+{
+    private $codigo;
+    private $nombres;
+    private $apellidos;
+
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+    public function setCodigo($value)
+    {
+        $this->codigo = $value;
+    }
+
+    public function getNombres()
+    {
+        return $this->nombres;
+    }
+    public function setNombres($value)
+    {
+        $this->nombres = $value;
+    }
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+    public function setApellidos($value)
+    {
+        $this->apellidos = $value;
+    }
+}
